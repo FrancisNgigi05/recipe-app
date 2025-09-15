0e4c15fdc767bbcaf3cb958e4f214d62589a9b43
FrancisNgigi05 <ngugifrancis930@gmail.com>
2025-09-15 08:59:27 +0300

    Done

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://recipe-app-6-85cq.onrender.com/recipes"
+    ? "https://recipe-app-6-85cq.onrender.com"
     : "http://localhost:5000/recipes";
 
 export { API_URL };
